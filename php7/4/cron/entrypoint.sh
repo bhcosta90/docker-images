ada9df32ff9c26ab24959b7792fc7833a739035b
Bruno Costa <bruno.costa@superlogica.com>
2020-05-19 11:06:18 -0300

    finalizado

--- php7/4/cron/entrypoint.sh
+++ php7/4/cron/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # supervisord --nodaemon --configuration /etc/supervisord.conf
-supervisord --configuration /etc/supervisord.conf
+# supervisord --nodaemon --configuration /etc/supervisord.conf
+supervisord -c /etc/supervisord.conf
